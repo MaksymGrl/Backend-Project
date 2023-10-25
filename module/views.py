b341167801f490978c0f7243102cf87ca4ea763a
Girlya Maskym <maxgirlya@gmail.com>
2023-10-25 23:03:18 +0300

    Healthcheck timestamp added

--- module/views.py
+++ module/views.py
@@ -1,9 +1,18 @@
 from module import app
+from datetime import datetime
+from flask import make_response
+import json
+
 
 @app.route("/")
 def home():
-    return "Hello, World"
+    return "Backend Project Maksym G."
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
-    return 'OK', 200
+    response = {
+        'status': 'UP',
+        'timestamp': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    }
+    response = json.dumps(response, indent=4)
+    return make_response((response, 200, {'Content-Type': 'application/json'}))
